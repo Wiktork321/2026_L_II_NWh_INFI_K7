77ddca094d0e86356bbbff1c840d6077b1eb6043
Wiktork321 <wiktork23.00@gmail.com>
2026-05-15 13:02:13 +0200

    Zmiana imienia i poprawa formatu JSON

--- test/test_views.py
+++ test/test_views.py
@@ -1,18 +1,11 @@
 import unittest
+
 from hello_world import app
-from hello_world.formater import SUPPORTED
 
 
-class FlaskrTestCase(unittest.TestCase):
-    def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
+class TestViews(unittest.TestCase):
 
-    def test_outputs(self):
-        rv = self.app.get('/outputs')
-        s = str(rv.data)
-        ','.join(SUPPORTED) in s
-
-    def test_msg_with_output(self):
-        rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+    def test_index_json(self):
+        tester = app.test_client(self)
+        rv = tester.get('/?output=json')
+        self.assertEqual(b'{ "imie":"Wiktor", "msg":"Hello World!"}', rv.data)
